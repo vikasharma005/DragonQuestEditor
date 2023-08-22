4ad97243291f56bf150e20c794b37259dc31aed0
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2023-08-22 11:12:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,30 +57,30 @@ tileset = {
     "@": "https://oshi.at/ZMUu/avRY.gif",
     "W": "https://thumbs2.imgbox.com/10/db/7zaxbIP8_t.png",  # wall
     "FP": "https://thumbs2.imgbox.com/29/22/5rTLr6WH_t.png",  # floor_plain
-    "CAT": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/cat.gif",  # cat
-    "M": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/monster.gif",  # monster, skeleton
-    "FS": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_stain_1.png",
+    "CAT": "https://github.com/vikasharma005/DragonQuest/blob/main/graphics/other/cat.gif",  # cat
+    "M": "https://github.com/vikasharma005/DragonQuest/blob/main/graphics/other/monster.gif",  # monster, skeleton
+    "FS": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/floor_stain_1.png",
     "E": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
-    "FE3": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_edge_3.png",  # floor_edge_3
-    "WON": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_n.png",  # Wall_outer_n
-    "WOE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_e.png",  # Wall_outer_e
-    "WONE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_ne.png",  # Wall_outer_ne
-    "WOW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_w.png",  # Wall_outer_w
-    "WONW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_nw.png",  # wall_outer_nw
-    "WFR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_front_right.png",  # wall front right
+    "FE3": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/floor_edge_3.png",  # floor_edge_3
+    "WON": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/Wall_outer_n.png",  # Wall_outer_n
+    "WOE": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/Wall_outer_e.png",  # Wall_outer_e
+    "WONE": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/Wall_outer_ne.png",  # Wall_outer_ne
+    "WOW": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/Wall_outer_w.png",  # Wall_outer_w
+    "WONW": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/Wall_outer_nw.png",  # wall_outer_nw
+    "WFR": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/Wall_front_right.png",  # wall front right
     "WTR": "https://oshi.at/QpWg/Mfxv.png",  # wall top right
-    # "DK": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # darkness
-    "WMB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # wall missing brick 1
-    "BOX": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/box.png",  # box
-    "DR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_right.png",  # darkenss right
-    "DB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_bottom.png",  # darkness bottom
-    "T": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/torch.gif",  # torch
-    "FMN1": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_1.png",  # floor_mud_n_1
-    "FMN2": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_2.png",  # floor_mud_n_2
-    "FMNE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_ne.png",  # floor_mud_ne
-    "CGOF": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_golden_open_full.png",  # chest_golden_open_full
-    "CGOO": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_open_empty.png",  # chest_open_empty
-    "FL": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Floor_ladder.png",  # Floor_ladder
+    # "DK": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # darkness
+    "WMB": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # wall missing brick 1
+    "BOX": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/box.png",  # box
+    "DR": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/darkness_right.png",  # darkenss right
+    "DB": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/darkness_bottom.png",  # darkness bottom
+    "T": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/other/torch.gif",  # torch
+    "FMN1": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/floor_mud_n_1.png",  # floor_mud_n_1
+    "FMN2": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/floor_mud_n_2.png",  # floor_mud_n_2
+    "FMNE": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/floor_mud_ne.png",  # floor_mud_ne
+    "CGOF": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/chest_golden_open_full.png",  # chest_golden_open_full
+    "CGOO": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/chest_open_empty.png",  # chest_open_empty
+    "FL": "https://raw.githubusercontent.com/vikasharma005/streamlit-dungeon/main/graphics/tileset/Floor_ladder.png",  # Floor_ladder
 }
 
 
@@ -97,7 +97,7 @@ tileset = {
 local_css("style.css")
 
 # title
-st.subheader("The Dungeon level editor")
+st.subheader("The DragonQuest level editor")
 
 # file upload for ones wanting to upload their own file
 uploaded_file = st.file_uploader("Choose a file")
